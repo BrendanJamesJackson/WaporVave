9aa667f8718c26a997c629460bd1e45be31c38d8
Brendan Jackson <brendanjj50@gmail.com>
2022-07-22 11:06:48 +0200

    Repeat Environment

--- Assets/Scripts/repeat_environment.cs
+++ Assets/Scripts/repeat_environment.cs
@@ -7,6 +7,10 @@ public class repeat_environment : MonoBehaviour
 
     public GameObject[] environments_array;
 
+    public GameObject[] environment_prefab;
+
+    public float environment_offset;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,19 @@ public class repeat_environment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void enviro_update()
+    {
+        Destroy(environments_array[0]);
+        environments_array[0] = environments_array[1];
+        environments_array[1] = environments_array[2];
+        AddEnvironment();
+    }
+
+    void AddEnvironment()
+    {
+        environments_array[2] = Instantiate(environment_prefab[Random.Range(0,environment_prefab.Length)], environments_array[1].transform.position + new Vector3(environment_offset,0f,0f), Quaternion.identity);
     }
 }
